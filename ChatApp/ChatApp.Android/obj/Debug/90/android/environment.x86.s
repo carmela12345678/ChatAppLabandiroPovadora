7ad947c8831605a5db98142e7718763253f0624c
carmela12345678 <77949987+carmela12345678@users.noreply.github.com>
2022-05-14 15:31:05 +0800

    try

--- ChatApp/ChatApp.Android/obj/Debug/90/android/environment.x86.s
+++ ChatApp/ChatApp.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"40c3e5fa-eed9-4e73-8c8d-f3c50fec6cb4"
+	.asciz	"282f7b53-bc52-40e1-a283-11e71cbf3d25"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
